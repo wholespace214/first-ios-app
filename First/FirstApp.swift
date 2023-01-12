3beb0b7eec31a8e404e5c8236ec208441265d286
wholespace214 <wholespace.214>
2023-01-12 03:42:38 -0800

    init commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Space on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
