62d02332fa8ed7d767af453688bb41b9ce491d82
wholespace214 <wholespace.214@gmail.com>
2023-01-16 00:43:31 +0000

    building lists... & section 4 completed

---
+++ First/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  First
+//
+//  Created by ec2-user on 1/16/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
